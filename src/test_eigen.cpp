ff7ea03693001e122c535c22915833fea607989c
liuwenxue <andy.liu@zichuan-tech.com>
2024-09-24 17:07:53 +0800

    测试了一下eigen矩阵的用法

--- src/test_eigen.cpp
+++ src/test_eigen.cpp
@@ -37,5 +37,89 @@ int main(int argc, char**argv)
     matrixXd << Eigen::MatrixXd::Identity(row, col);
     std::cout << matrixXd << std::endl << std::endl;
 
+    // 2024-9-24
+    using MatX = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+    using VecX = Eigen::Matrix<double, Eigen::Dynamic, 1>;
+    Eigen::Matrix<double, 6, 36> Q2Jp;
+    Q2Jp.setOnes();
+    VecX tempVector;
+    tempVector.resize(36 + 1);
+    double* tempData = tempVector.data();
+    double hCoeff=1.0;
+    Q2Jp.bottomRightCorner(1, 30).applyHouseholderOnTheLeft(Q2Jp.col(5).tail(1 - 1),hCoeff, tempData + 5 + 1);
+    std::cout << Q2Jp << std::endl;
+    std::cout << "0 \n" << Q2Jp.bottomRightCorner(0, 0) << std::endl;
+    std::cout << "1 \n" << Q2Jp.col(5).tail(1 - 1) << std::endl;
+    std::cout << "2 \n" << Q2Jp.col(5).tail(2 - 1) << std::endl;
+    std::cout << "3 \n" << Q2Jp.col(5).tail(3 - 1) << std::endl;
+    // std::cout << Q2Jp.bottomRightCorner(-1, 30) << std::endl;
+    try
+    {
+        /* code */
+        std::cout << Q2Jp.bottomRightCorner(1, 37) << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch(...)
+    {
+        std::cout << "catch ....\n";
+    }
+    
+    
+
+    Eigen::Index marg_rank = 0;
+    Eigen::Index total_rank = 0;
+    {
+    const double rank_threshold =
+        std::sqrt(std::numeric_limits<double>::epsilon()); // epsilon=2.22045e-16一个很小的小量
+
+    const Eigen::Index rows = Q2Jp.rows();
+    const Eigen::Index cols = Q2Jp.cols();
+
+    VecX tempVector;
+    tempVector.resize(cols + 1);
+    double* tempData = tempVector.data();
+
+    for (Eigen::Index k = 0; k < cols && total_rank < rows; ++k) {
+      Eigen::Index remainingRows = rows - total_rank;
+      Eigen::Index remainingCols = cols - k - 1;
+
+      double beta;
+      double hCoeff;
+      Q2Jp.col(k).tail(remainingRows).makeHouseholderInPlace(hCoeff, beta);
+
+    //   if (std::abs(beta) > rank_threshold) {
+      if (1) {
+        Q2Jp.coeffRef(total_rank, k) = beta;
+
+        // // Assertion failed 2024-9-20
+        {
+          char szLog[255] = { 0 };
+          sprintf(szLog, "rows=%d,cols=%d,remainingRows=%d,remainingCols=%d, k=%d", rows, cols, remainingRows, remainingCols, k);
+        //   wx::TFileSystemHelper::WriteLog(szLog);
+            std::cout << szLog << std::endl;
+        }
+        // the end.
+        Q2Jp.bottomRightCorner(remainingRows, remainingCols)
+            .applyHouseholderOnTheLeft(Q2Jp.col(k).tail(remainingRows - 1),
+                                       hCoeff, tempData + k + 1);
+        // Q2r.tail(remainingRows)
+        //     .applyHouseholderOnTheLeft(Q2Jp.col(k).tail(remainingRows - 1),
+        //                                hCoeff, tempData + cols);
+        total_rank++;
+      } else {
+          std::cout << "2 k=" << k << std::endl;
+        Q2Jp.coeffRef(total_rank, k) = 0;
+      }
+
+      // Overwrite householder vectors with 0
+      Q2Jp.col(k).tail(remainingRows - 1).setZero();
+
+      
+    }
+  }
+
     return 0;
 }
